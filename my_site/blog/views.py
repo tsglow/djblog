2afba71300802db47e7a7e8fadbf8c26b5174e11
dinner_rolls <haruki84@msn.com>
2026-05-21 11:10:42 +0900

    feat: views modified

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.utils.text import slugify
+from .models import Author, Post
 from datetime import date
+
 
 post_list = [
     {"date": date(2026,4,11), "author": "max", "title": "Time flys like an arrow", "image": "mountains.jpg","content": "why time flys like an arrow, not a bunch of banana, or apple?"},
@@ -18,28 +20,20 @@ def get_date(post):
 # Create your views here.
 
 def index(request):
-    sorted_post_list = sorted(post_list,key=get_date)
-    print("sorted", sorted_post_list)  
-    recents = sorted_post_list[-2:][::-1]    
-    for r in recents:
-        if r.get("url") is None:
-            r["url"] = slugify(r["title"])    
+    posts = Post.objects.all().order_by("-date")            
+    print(posts)  
+    recents = posts[:2]        
     return render(request, "blog/index.html", {"recent": recents})
 
 
 def posts(request):
-    sorted_post_list = sorted(post_list,key=get_date)
-    for p in sorted_post_list:
-        if p.get("url") is None:
-            p["url"] = slugify(p["title"])
-    print(post_list)
-    return render(request,"blog/posts.html", {"list": sorted_post_list} )
+    posts = Post.objects.all().order_by("-date")    
+    return render(request,"blog/posts.html", {"list": posts} )
 
 
 def read_post(request,url):
-    try:        
-        post = list(filter(lambda i : i['url'] == url, post_list))[0]
-        # post = next( post for post in post_list if post['url'] == url)        
+    try:   
+        post = Post.objects.get(url=url)
         return render(request, "blog/post.html", {"post": post})       
     except:        
         raise Http404()
