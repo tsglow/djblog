6e1eab7df450525f39a2ff38633a45f245058e7d
dinner_rolls <haruki84@msn.com>
2026-05-21 10:47:19 +0900

    feat: models added

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Author, Post
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name")
+    list_display = ("first_name", "last_name")
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"url": ("title",)}
+    list_filter = ("title", "author")
+    list_display = ("title", "author", "date")
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    date = models.DateTimeField(auto_now_add=True)    
+    image = models.CharField(max_length=100)
+    content = models.TextField()    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="post")
+    url = models.SlugField(default="", null=False, db_index=True)
+    
+
+    def save(self, *args, **kwargs):
+        self.url = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def post_name(self):
+        return f"{self.title}"
+        
+    def __str__(self):
+        return self.post_name()
