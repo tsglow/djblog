aa875239e192a8ebc6ba0eb94521df0a6c226d59
dinner_rolls <haruki84@msn.com>
2026-05-26 13:24:28 +0900

    feat: Tag model added

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Author, Post
+from .models import Author, Post, Tag
 # Register your models here.
 
 class AuthorAdmin(admin.ModelAdmin):
     list_filter = ("first_name", "last_name")
     list_display = ("first_name", "last_name")
+
+class TagAdmin(admin.ModelAdmin):
+    list_filter = ("caption",)
+    list_display = ("caption",)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"url": ("title",)}
@@ -13,3 +17,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -16,11 +25,12 @@ class Author(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200, db_index=True)
-    date = models.DateTimeField(auto_now_add=True)    
+    date = models.DateTimeField(auto_now=True)    
     image = models.CharField(max_length=100)
-    content = models.TextField()    
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="post")
-    url = models.SlugField(default="", null=False, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(4)])    
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="post")
+    url = models.SlugField(unique=True, default="", null=False) # unique filed automatically index=True
+    tag = models.ManyToManyField(Tag)
     
 
     def save(self, *args, **kwargs):
@@ -32,3 +42,4 @@ class Post(models.Model):
         
     def __str__(self):
         return self.post_name()
+
